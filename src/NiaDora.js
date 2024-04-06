881349305773689ea14a4f69d642d9c3c77bce25
EC2 Default User <ec2-user@ip-192-168-0-213.ap-northeast-1.compute.internal>
2024-04-06 09:53:55 +0000

    gohanmae

--- src/NiaDora.js
+++ src/NiaDora.js
@@ -2,35 +2,43 @@ import React, { useState } from 'react';
 import './Table.css'; // 外部のCSSファイルをインポート
 
 function NiaDora() {
+  // メモ
+  // オリンピックスコアは18要素の配列として扱う。
+  // ニアドラスコアは8要素の配列として扱う。（ニア1~4,ドラ1~4）
   const [players, setPlayers] = useState([
-    { name: 'Player 1', scores: Array(18).fill(0) },
-    { name: 'Player 2', scores: Array(18).fill(0) },
-    { name: 'Player 3', scores: Array(18).fill(0) },
-    { name: 'Player 4', scores: Array(18).fill(0) }
+    { name: 'Player 1', olympicScores: Array(18).fill(0), olympicSoshy: 0, niadoraScores: Array(8).fill(0), niadoraSoshy: 0},
+    { name: 'Player 2', olympicScores: Array(18).fill(0), olympicSoshy: 0, niadoraScores: Array(8).fill(0), niadoraSoshy: 0},
+    { name: 'Player 3', olympicScores: Array(18).fill(0), olympicSoshy: 0, niadoraScores: Array(8).fill(0), niadoraSoshy: 0},
+    { name: 'Player 4', olympicScores: Array(18).fill(0), olympicSoshy: 0, niadoraScores: Array(8).fill(0), niadoraSoshy: 0}
   ]);
+
   const rate = 300;
 
   // 合計スコアを計算する関数
-  const calculateTotalScore = (playerScores) => {
-    const totalScore = playerScores.reduce((acc, cur) => acc + parseInt(cur), 0);
-    return totalScore;
+  const calculateTotalOlympicScore = (playerOlympicScores) => {
+    const totalOlympicScore = playerOlympicScores.reduce((acc, cur) => acc + parseInt(cur), 0);
+    return totalOlympicScore;
   };
 
   // オリンピックのスコアを計算する関数
   const calculateOlympicScore = (players, playerIndex) => {
     // 全プレイヤーの合計スコア
-    const totalScores = players.map((player) => calculateTotalScore(player.scores));
-    const totalScoreOfAllPlayers = totalScores.reduce((acc, cur) => acc + cur, 0);
-    const totalScoreOfPlayer = totalScores[playerIndex];
-    const olympicScore = totalScoreOfPlayer * 4 - totalScoreOfAllPlayers;
-    return olympicScore;
+    const totalOlympicScores = players.map((player) => calculateTotalOlympicScore(player.olympicScores));
+    const totalOlympicScoreOfAllPlayers = totalOlympicScores.reduce((acc, cur) => acc + cur, 0);
+    const totalOlympicScoreOfPlayer = totalOlympicScores[playerIndex];
+    const olympicOlympicScore = totalOlympicScoreOfPlayer * 4 - totalOlympicScoreOfAllPlayers;
+    return olympicOlympicScore;
   };
 
 
   // スコアを更新する関数
-  const updateScore = (playerIndex, holeIndex, value) => {
+  const updateOlympicScore = (playerIndex, holeIndex, value) => {
     const newPlayers = [...players];
-    newPlayers[playerIndex].scores[holeIndex] = value;
+    newPlayers[playerIndex].olympicScores[holeIndex] = value;
+    for(let i = 0; i < newPlayers.length; i++){
+      newPlayers[i].olympicSoshy = calculateOlympicScore(newPlayers, i) * rate;
+    }
+    // newPlayers[playerIndex].olympicSoshy = calculateOlympicScore(newPlayers, playerIndex) * rate;
     setPlayers(newPlayers);
   };
 
@@ -43,7 +51,6 @@ function NiaDora() {
 
   return (
     <div>
-      <h1>オリンピック</h1>
       <div className='table-container'>
         <table>
           <thead>
@@ -62,17 +69,18 @@ function NiaDora() {
             </tr>
           </thead>
           <tbody className="scrollable-content">
+            <tr><td colSpan="5">オリンピック</td></tr>
             {[...Array(18)].map((_, holeIndex) => (
               <tr key={holeIndex}>
                 <td className="fixed-column">{holeIndex + 1}</td>
                 {players.map((player, playerIndex) => (
                   <td key={playerIndex}>
                     <select
-                      value={player.scores[holeIndex]}
-                      onChange={(e) => updateScore(playerIndex, holeIndex, e.target.value)}
+                      value={player.olympicScores[holeIndex]}
+                      onChange={(e) => updateOlympicScore(playerIndex, holeIndex, e.target.value)}
                     >
-                      {[0, 1, 2, 3, 4, 5].map((score) => (
-                        <option key={score} value={score}>{score}</option>
+                      {[0, 1, 2, 3, 4, 5].map((olympicScore) => (
+                        <option key={olympicScore} value={olympicScore}>{olympicScore}</option>
                       ))}
                     </select>
                   </td>
@@ -83,7 +91,7 @@ function NiaDora() {
             <tr>
               <td className="fixed-column">合計</td>
               {players.map((player, playerIndex) => (
-                <td key={playerIndex}>{calculateTotalScore(player.scores)}</td>
+                <td key={playerIndex}>{calculateTotalOlympicScore(player.olympicScores)}</td>
               ))}
             </tr>
             {/* オリンピックのスコアを追加 */}
@@ -97,7 +105,8 @@ function NiaDora() {
             <tr>
               <td className="fixed-column">ｿｼｰ</td>
               {players.map((player, playerIndex) => (
-                <td key={playerIndex}>{calculateOlympicScore(players, playerIndex) * rate} </td>
+                // <td key={playerIndex}>{calculateOlympicScore(players, playerIndex) * rate} </td>
+                <td key={playerIndex}>{player.olympicSoshy} </td>
               ))}
             </tr>
           </tbody>
